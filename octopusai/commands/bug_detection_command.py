d96697f6d710d181cc63edf95efb41871b189979
pkunray <pkunray@gmail.com>
2025-08-29 00:08:58 +0200

    feat: add option for choosing process mode

--- octopusai/commands/bug_detection_command.py
+++ octopusai/commands/bug_detection_command.py
@@ -1,15 +1,17 @@
 import click
-import octopusai.crews.bug_detection_flow as bug_detection_flow
+import octopusai.crews.bug_detection_flow as sequential
+import octopusai.crews.bug_detection_hierarchical as hierarchical
 from crewai_tools import MCPServerAdapter
-import octopusai.crews.bug_detection_flow as bug_detection_flow
+
 
 @click.command("bug")
 @click.argument("repo")
 @click.argument("pr_number")
 @click.argument("active_branch")
 @click.option("--requirement_id", "-r", help="Requirement ID for the Pull Request (PR)")
+@click.option("--mode", "-m", type=click.Choice(["sequential", "hierarchical"]), default="sequential", help="Choose the bug detection mode")
 @click.pass_context
-def bug_detection(ctx: click.Context, repo: str, pr_number: str, active_branch: str, requirement_id: str):
+def bug_detection(ctx: click.Context, repo: str, pr_number: str, active_branch: str, requirement_id: str, mode: str):
     """Run the bug detection workflow."""
     click.echo("Running Bug Detection Workflow...")
     inputs={
@@ -19,5 +21,10 @@ def bug_detection(ctx: click.Context, repo: str, pr_number: str, active_branch: 
         "requirement_id": requirement_id,
     }
     click.echo(f"Inputs: {inputs}")
-    with MCPServerAdapter(bug_detection_flow.BugDetectionFlow.mcp_server_params) as mcp_tools:
-        bug_detection_flow.main(inputs=inputs, mcp_tools=mcp_tools)
+
+    if mode == "sequential":
+        with MCPServerAdapter(sequential.BugDetectionFlow.mcp_server_params) as mcp_tools:
+            sequential.main(inputs=inputs, mcp_tools=mcp_tools)
+    else:
+        with MCPServerAdapter(hierarchical.BugDetectionFlow.mcp_server_params) as mcp_tools:
+            hierarchical.main(inputs=inputs, mcp_tools=mcp_tools)
